1548a711e47056cc74204ab892b1d387f9effaf9
ssophii <ssophinaa@gmail.com>
2024-11-20 23:54:52 +0700

    lp, lgn, rgstr, -logo

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,54 +1,190 @@
-<x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
 
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
+<!DOCTYPE html>
+<html lang="en">
 
-        <!-- Email Address -->
-        {{-- <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div> --}}
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <title>Website Keuangan ERCOM</title>
+  <meta name="description" content="">
+  <meta name="keywords" content="">
 
-        <!-- NPM -->
-        <div>
-            <x-input-label for="npm" :value="__('NPM')" />
-            <x-text-input id="npm" class="block mt-1 w-full" type="text" name="npm" :value="old('npm')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('npm')" class="mt-2" />
-        </div>
+  <!-- Favicons -->
+  <link rel="shortcut icon" href="{{ asset('assets/images/qrcode.svg') }}">
+    {{-- <link href="{{ asset('assets/img/logo-unib.png') }}" rel="icon">
+    <link href="{{ asset('assets/images/logo-himatif.png') }}" rel="shortcut icon"> --}}
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com" rel="preconnect">
+    <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&family=Poppins:wght@100;200;300;400;500;600;700;800;900&family=Raleway:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
+    <!-- Vendor CSS Files -->
+    <link href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/vendor/aos/aos.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('assets/vendor/feather/feather.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/vendor/ti-icons/css/themify-icons.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/vendor/css/vendor.bundle.base.css') }}">
 
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
+    <!-- Main CSS File -->
+    <link href="{{ asset('assets/css/main.css') }}" rel="stylesheet">
 
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-            </label>
-        </div>
+    <!-- Injected Styles -->
+    <link rel="stylesheet" href="{{ asset('assets/css/vertical-layout-light/style.css') }}">
 
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
+</head>
 
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+<body class="index-page">
+
+  <!-- Header Section -->
+  <header id="header" class="header d-flex align-items-center fixed-top">
+    <div class="container-fluid container-xl position-relative d-flex align-items-center justify-content-between">
+
+      <a href="{{ url('/') }}" class="logo d-flex align-items-center">
+        <!-- Uncomment the line below if you also wish to use an image logo -->
+        <img src="{{ asset('assets/images/ercom-mini.png') }}" alt="">
+        <h1 class="sitename">Website Keuangan ERCOM</h1>
+      </a>
+
+      <nav id="navmenu" class="navmenu">
+        <ul>
+          <li><a href="{{ url('/') }}" >Home</a></li>
+          <li><a href="{{ url('login') }}" class="active">Login</a></li>
+          <li><a href="{{ url('register') }}" >Register</a></li>
+        </ul>
+        <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+      </nav>
+
+    </div>
+  </header>
+  <!-- End Header -->
+
+  <main class="main">
+
+    <!-- Hero Section -->
+    <section id="hero" class="hero section dark-background">
+
+      <div class="container">
+        <div class="row w-100 mx-0 ">
+            <div class="col-lg-4 mx-auto">
+              <div class="auth-form-dark text-left py-5 px-4 px-sm-5 bg-white shadow rounded-lg">
+                <div class="brand-logo">
+                  <div class="text-center mb-4">
+                      <img src="{{ asset('assets/images/logo-himatif.png') }}" alt="logo" class="logo-center">
+                  </div>
+                </div>
+                <h4 class="text-dark">Hello! Ayo Masuk</h4>
+                <h6 class="font-weight-light text-secondary">Login untuk melanjutkan.</h6>
+  
+                <!-- Form Start -->
+                <form method="POST" action="{{ route('login') }}" class="pt-3">
+                  @csrf
+  
+                  <!-- NPM -->
+                  <div class="form-group">
+                    <input type="text" id="npm" class="form-control form-control-lg" name="npm" :value="old('npm')" required autofocus placeholder="NPM">
+                    @error('npm')
+                      <span class="text-danger">{{ $message }}</span>
+                    @enderror
+                  </div>
+  
+                  <!-- Password -->
+                  <div class="form-group">
+                    <input type="password" id="password" class="form-control form-control-lg" name="password" required placeholder="Password">
+                    @error('password')
+                      <span class="text-danger">{{ $message }}</span>
+                    @enderror
+                  </div>
+  
+                  <!-- Login Button -->
+                  <div class="mt-3">
+                    <button type="submit" class="btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn">
+                      MASUK
+                    </button>
+                  </div>
+  
+                  <!-- Forgot Password -->
+                  <div class="my-2 d-flex justify-content-between align-items-center">
+                    @if (Route::has('password.request'))
+                      <a class="auth-link text-black" href="{{ route('password.request') }}">
+                        Forgot password?
+                      </a>
+                    @endif
+                  </div>
+  
+                  <!-- Register Link -->
+                  <div class="text-dark text-center mt-4 font-weight-light">
+                    Belum Punya Akun? <a href="{{ route('register') }}" class="text-primary">Buat</a>
+                  </div>
+                </form>
+                <!-- Form End -->
+  
+              </div>
+            </div>
+          </div>
+      </div>
+
+      <svg class="hero-waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 28 " preserveAspectRatio="none">
+        <defs>
+          <path id="wave-path" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"></path>
+        </defs>
+        <g class="wave1">
+          <use xlink:href="#wave-path" x="50" y="3"></use>
+        </g>
+        <g class="wave2">
+          <use xlink:href="#wave-path" x="50" y="0"></use>
+        </g>
+        <g class="wave3">
+          <use xlink:href="#wave-path" x="50" y="9"></use>
+        </g>
+      </svg>
+
+    </section><!-- End Hero Section -->
+
+  </main><!-- End Main -->
+
+  <!-- Footer Section -->
+  <footer id="footer" class="footer dark-background">
+    <div class="container footer-top">
+      <!-- Add any footer content here if necessary -->
+    </div>
+
+    <div class="container copyright text-center mt-4">
+      <p><span>Administrasi dan Keuangan HIMATIF 2024</span></p>
+    </div>
+  </footer><!-- End Footer -->
+
+  <!-- Scroll Top -->
+  <a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Preloader -->
+  <div id="preloader"></div>
+
+  <!-- Vendor JS Files -->
+    <script src="{{ asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('assets/vendor/php-email-form/validate.js') }}"></script>
+    <script src="{{ asset('assets/vendor/aos/aos.js') }}"></script>
+    <script src="{{ asset('assets/vendor/glightbox/js/glightbox.min.js') }}"></script>
+    <script src="{{ asset('assets/vendor/purecounter/purecounter_vanilla.js') }}"></script>
+    <script src="{{ asset('assets/vendor/swiper/swiper-bundle.min.js') }}"></script>
+
+    <!-- Vendor Bundle (Optional) -->
+    <script src="{{ asset('assets/vendor/js/vendor.bundle.base.js') }}"></script>
+
+    <!-- Main JS File -->
+    <script src="{{ asset('assets/js/main.js') }}"></script>
+
+    <!-- Injected Scripts -->
+    <script src="{{ asset('assets/js/off-canvas.js') }}"></script>
+    <script src="{{ asset('assets/js/hoverable-collapse.js') }}"></script>
+    <script src="{{ asset('assets/js/template.js') }}"></script>
+    <script src="{{ asset('assets/js/settings.js') }}"></script>
+    <script src="{{ asset('js/todolist.js') }}"></script>
+
+
+</body>
+
+</html>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Sistem Keuangan ERCOM</title>
+    <title>Website Keuangan ERCOM</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
     <!-- plugins:css -->
